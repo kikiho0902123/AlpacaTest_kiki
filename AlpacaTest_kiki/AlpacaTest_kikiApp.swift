9d0989e9ff5cac07f24bdf778d7dd913eb62877d
kiki <kikiho0902@gmail.com>
2026-08-13 00:08:32 +0800

    Initial Commit

---
+++ AlpacaTest_kiki/AlpacaTest_kikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlpacaTest_kikiApp.swift
+//  AlpacaTest_kiki
+//
+//  Created by kikiho on 2026/8/13.
+//
+
+import SwiftUI
+
+@main
+struct AlpacaTest_kikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
